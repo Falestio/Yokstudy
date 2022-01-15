30b2d5871766e0dbea765c087560e397c75a811c
Falestio <falestioltmpt@gmail.com>
2022-01-15 11:58:33 +0700

    make new blog list page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -7,6 +7,7 @@
             <div class="card-body">
                 <h2 class="card-title">Horizontal</h2> 
                 <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+            </div>
             <div class="card-actions">
                 <button class="btn btn-primary">Get Started</button> 
                 <button class="btn btn-ghost">More info</button>   
@@ -19,6 +20,7 @@
             <div class="card-body">
                 <h2 class="card-title">Horizontal</h2> 
                 <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+            </div>
             <div class="card-actions">
                 <button class="btn btn-primary">Get Started</button> 
                 <button class="btn btn-ghost">More info</button>   
@@ -31,6 +33,7 @@
             <div class="card-body">
                 <h2 class="card-title">Horizontal</h2> 
                 <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+            </div>
             <div class="card-actions">
                 <button class="btn btn-primary">Get Started</button> 
                 <button class="btn btn-ghost">More info</button>   
